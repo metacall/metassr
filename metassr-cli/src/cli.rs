27370fd58d580891e33b2db9a212334ba7555345
hulxv <hulxxv@gmail.com>
2024-04-25 21:56:00 +0200

    feat(cli): integerate with logger layer and impl http tracing

--- metassr-cli/src/cli.rs
+++ metassr-cli/src/cli.rs
@@ -1,5 +1,4 @@
 use clap::{command, Parser};
-use std::fmt::Display;
 #[derive(Parser, Debug)]
 #[command(
     author,
@@ -13,6 +12,17 @@ pub struct Args {
     /// The path of project root
     #[arg(long, default_value_t = String::from("."))]
     pub root: String,
+    /// the port where HTTP server running
     #[arg(long, default_value_t = 8080)]
     pub port: u16,
+    /// Run with debug mode (add more information with logs)
+    #[arg(long)]
+    pub debug_mode: bool,
+
+    /// The path where your logs want be saved
+    #[arg[long]]
+    pub log_file: Option<String>,
+    /// Show HTTP logs (requests & responses)
+    #[arg(long)]
+    pub enable_http_logging: bool,
 }
