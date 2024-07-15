cee0cc86e5e03ed43f4947210efce003579213b3
hulxv <hulxxv@gmail.com>
2024-07-16 02:30:14 +0300

    refactor(utils):  replace `fs_analyzer` with `src_analyzer` to be more accurate

--- crates/metassr-utils/src/lib.rs
+++ crates/metassr-utils/src/lib.rs
@@ -1,1 +1,1 @@
-pub mod fs_analyzer;
+pub mod src_analyzer;
--- crates/metassr-utils/src/src_analyzer.rs
+++ crates/metassr-utils/src/src_analyzer.rs
@@ -1,19 +1,24 @@
 use anyhow::Result;
-use std::{collections::HashMap, ffi::OsStr, marker::Sized, path::Path, process::Output};
+use std::{
+    collections::HashMap,
+    ffi::OsStr,
+    marker::Sized,
+    path::{Path, PathBuf},
+};
 use walkdir::WalkDir;
-trait AnalyzeDir {
+pub trait AnalyzeDir {
     type Output;
     fn analyze(&self) -> Result<Self::Output>;
 }
 
-#[derive(Debug)]
-struct SourceDirContainer {
-    pub pages: HashMap<String, String>,
-    pub specials: HashMap<String, Option<String>>,
+#[derive(Debug, Clone)]
+pub struct SourceDirContainer {
+    pub pages: HashMap<String, PathBuf>,
+    pub specials: HashMap<String, Option<PathBuf>>,
 }
 
 #[derive(Debug)]
-struct SourceDir<'a>(&'a Path);
+pub struct SourceDir<'a>(&'a Path);
 
 impl<'a> SourceDir<'a> {
     pub fn new<S>(path: &'a S) -> Self
@@ -29,14 +34,20 @@ impl<'a> AnalyzeDir for SourceDir<'a> {
     fn analyze(&self) -> Result<Self::Output> {
         let src = self.0.to_str().unwrap();
 
-        let mut pages: HashMap<String, String> = HashMap::new();
-        let mut specials: HashMap<String, Option<String>> =
+        let mut pages: HashMap<String, PathBuf> = HashMap::new();
+        let mut specials: HashMap<String, Option<PathBuf>> =
             HashMap::from([("_app".to_owned(), None), ("_head".to_owned(), None)]);
 
         for entry in WalkDir::new(src).into_iter().filter_map(|e| {
-            e.ok().and_then(|e| match e.path().is_file() {
-                true => Some(e),
-                false => None,
+            // Check if the entry is a js/ts file.
+            let exts: Vec<&str> = vec!["js", "jsx", "tsx", "ts"];
+            e.ok().and_then(|e| {
+                match e.path().is_file()
+                    && exts.contains(&e.path().extension().unwrap().to_str().unwrap())
+                {
+                    true => Some(e),
+                    false => None,
+                }
             })
         }) {
             let path = entry.path();
@@ -45,7 +56,7 @@ impl<'a> AnalyzeDir for SourceDir<'a> {
 
             match stripped.iter().next() {
                 Some(_) if specials.contains_key(stem) => {
-                    specials.insert(stem.to_owned(), Some(path.to_str().unwrap().to_owned()));
+                    specials.insert(stem.to_owned(), Some(path.to_path_buf()));
                 }
 
                 Some(p) if p == OsStr::new("pages") => {
@@ -53,8 +64,7 @@ impl<'a> AnalyzeDir for SourceDir<'a> {
                         .strip_prefix([src, "/pages"].concat())?
                         .to_str()
                         .unwrap();
-                    println!("{route}");
-                    pages.insert(route.to_owned(), path.to_str().unwrap().to_owned());
+                    pages.insert(route.to_owned(), path.to_path_buf());
                 }
 
                 _ => (),
