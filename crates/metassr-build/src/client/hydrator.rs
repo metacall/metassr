72e6bda0da7ac4e74e48079b884886ac405c4d35
hulxv <hulxxv@gmail.com>
2024-07-16 02:30:53 +0300

    refactor: use `PathBuf` instead of strings for paths in the `hydrator`

--- crates/metassr-build/src/client/hydrator.rs
+++ crates/metassr-build/src/client/hydrator.rs
@@ -1,3 +1,7 @@
+use std::{ffi::OsStr, path::PathBuf};
+
+use anyhow::Result;
+
 const HYDRATED_FILE_TEMPLATE: &str = include_str!("./scripts/hydrate.js.template");
 
 const APP_PATH_TAG: &str = "%APP_PATH%";
@@ -6,25 +10,34 @@ const ROOT_ID_TAG: &str = "%ROOT_ID%";
 
 #[derive(Debug, Clone)]
 pub struct Hydrator {
-    app_path: String,
-    page_path: String,
+    app_path: PathBuf,
+    page_path: PathBuf,
     root_id: String,
 }
 
 impl Hydrator {
-    pub fn new<'a>(app_path: &'a str, page_path: &'a str, root_id: &'a str) -> Self {
+    pub fn new<'a, S>(app_path: &'a S, page_path: &'a S, root_id: &'a str) -> Self
+    where
+        S: AsRef<OsStr> + ?Sized,
+    {
         Self {
-            app_path: app_path.to_string(),
-            page_path: page_path.to_string(),
+            app_path: PathBuf::from(app_path),
+            page_path: PathBuf::from(page_path),
             root_id: root_id.to_string(),
         }
     }
 
-    pub fn generate(&self) -> String {
-        HYDRATED_FILE_TEMPLATE
-            .replace(APP_PATH_TAG, &self.app_path)
-            .replace(PAGE_PATH_TAG, &self.page_path)
-            .replace(ROOT_ID_TAG, &self.root_id)
+    pub fn generate(&self) -> Result<String> {
+        Ok(HYDRATED_FILE_TEMPLATE
+            .replace(
+                APP_PATH_TAG,
+                self.app_path.canonicalize()?.to_str().unwrap(),
+            )
+            .replace(
+                PAGE_PATH_TAG,
+                self.page_path.canonicalize()?.to_str().unwrap(),
+            )
+            .replace(ROOT_ID_TAG, &self.root_id))
     }
 }
 
@@ -35,7 +48,9 @@ mod tests {
     fn generate_hydrated_file() {
         println!(
             "{}",
-            Hydrator::new("src/_app.tsx", "src/pages/home.jsx", "root").generate()
+            Hydrator::new("src/_app.tsx", "src/pages/home.jsx", "root")
+                .generate()
+                .unwrap()
         );
     }
 }
