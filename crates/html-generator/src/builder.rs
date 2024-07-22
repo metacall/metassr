8291411e0706dd5bfb2b65e06e38b1e85d67930b
hulxv <hulxxv@gmail.com>
2024-07-22 18:13:45 +0300

    feat(html-gen): able to save the html output to a file

--- crates/html-generator/src/builder.rs
+++ crates/html-generator/src/builder.rs
@@ -1,26 +1,44 @@
 use super::{html_props::HtmlProps, template::HtmlTemplate};
+use anyhow::Result;
+use std::{fs::File, io::Write, path::PathBuf};
 
 const LANG_TAG: &str = "%LANG%";
 const HEAD_TAG: &str = "%HEAD%";
 const BODY_TAG: &str = "%BODY%";
 const SCRIPTS_TAG: &str = "%SCRIPTS%";
 const STYLES_TAG: &str = "%STYLES%";
-pub struct HtmlBuilder<'a> {
-    template: HtmlTemplate,
-    props: HtmlProps<'a>,
+
+#[derive(Debug, Clone)]
+pub struct HtmlOutput(String);
+
+impl HtmlOutput {
+    pub fn from(html: &str) -> Self {
+        Self(html.to_string())
+    }
+
+    pub fn write(&self, path: PathBuf) -> Result<()> {
+        let mut file = File::create(path)?;
+        file.write_all(self.0.as_bytes())?;
+        Ok(())
+    }
 }
 
-impl<'a> HtmlBuilder<'a> {
-    pub fn new(template: HtmlTemplate, props: HtmlProps<'a>) -> Self {
+pub struct HtmlBuilder {
+    template: HtmlTemplate,
+    props: HtmlProps,
+}
+
+impl HtmlBuilder {
+    pub fn new(template: HtmlTemplate, props: HtmlProps) -> Self {
         Self { template, props }
     }
 
-    pub fn generate(&self) -> String {
+    pub fn generate(&self) -> HtmlOutput {
         let scripts = self
             .props
             .scripts
             .iter()
-            .map(|path| format!("<script src=\"{}\"></scripts>", path.display()))
+            .map(|path| format!("<script src=\"{}\"></script>", path.display()))
             .collect::<Vec<String>>()
             .join("");
 
@@ -31,13 +49,17 @@ impl<'a> HtmlBuilder<'a> {
             .map(|path| format!("<link rel=\"stylesheet\" href=\"{}\">", path.display()))
             .collect::<Vec<String>>()
             .join("");
-        self.template
-            .to_string()
-            .replace(LANG_TAG, &self.props.lang)
-            .replace(HEAD_TAG, &self.props.head)
-            .replace(BODY_TAG, &self.props.body)
-            .replace(SCRIPTS_TAG, &scripts)
-            .replace(STYLES_TAG, &styles)
+
+        HtmlOutput::from(
+            &self
+                .template
+                .to_string()
+                .replace(LANG_TAG, &self.props.lang)
+                .replace(HEAD_TAG, &self.props.head)
+                .replace(BODY_TAG, &self.props.body)
+                .replace(SCRIPTS_TAG, &scripts)
+                .replace(STYLES_TAG, &styles),
+        )
     }
 }
 
@@ -59,10 +81,10 @@ mod tests {
 <title>This is a static page</title>
         ",
             )
-            .scripts(vec!["main.js", "react.js"])
-            .styles(vec!["style.css"]);
+            .scripts(vec!["main.js".to_owned(), "react.js".to_owned()])
+            .styles(vec!["style.css".to_owned()]);
         let props = binding.build();
         let html = HtmlBuilder::new(HtmlTemplate::default(), props).generate();
-        println!("{html}")
+        println!("{html:?}")
     }
 }
