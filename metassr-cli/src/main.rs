41618c6041add5c1d35a7f9ec8584d385794b76b
hulxv <hulxxv@gmail.com>
2024-07-24 01:39:24 +0300

    feat(cli): add subcommands for building and running

--- metassr-cli/src/main.rs
+++ metassr-cli/src/main.rs
@@ -1,21 +1,29 @@
 mod cli;
 use clap::Parser;
-use cli::Args;
+use cli::{Args, Commands, DebugMode};
 use logger::LoggingLayer;
 
-use anyhow::Result;
-use metacall::{loaders, metacall, switch};
+use anyhow::{anyhow, Result};
+use metacall::switch;
+
+use metassr_build::{client::ClientBuilder, server::ServerSideBuilder, traits::Build};
 use metassr_core::server::{Server, ServerConfigs};
+
 use std::{
-    env::{current_dir, set_current_dir},
+    env::{current_dir, set_current_dir, set_var},
     path::Path,
+    time::Instant,
 };
 
+use tracing::{error, info};
 use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
+    let allow_metacall_debug =
+        [Some(DebugMode::All), Some(DebugMode::Metacall)].contains(&args.debug_mode);
+    let allow_http_debug = [Some(DebugMode::All), Some(DebugMode::Http)].contains(&args.debug_mode);
 
     tracing_subscriber::registry()
         .with(LoggingLayer {
@@ -27,16 +35,50 @@ async fn main() -> Result<()> {
     set_current_dir(&project_root)
         .map_err(|err| eprintln!("Cannot chdir: {err}"))
         .unwrap();
+    if allow_metacall_debug {
+        set_var("METACALL_DEBUG", "1");
+    }
 
-    let _metacall = switch::initialize().unwrap();
-    loaders::from_single_file("ts", ["src/_app.tsx"].concat()).unwrap();
+    match args.commands {
+        Some(Commands::Build { out_dir }) => {
+            let instant = Instant::now();
+            let _metacall = switch::initialize().unwrap();
+            if let Err(e) = ClientBuilder::new("", &out_dir)?.build() {
+                error!(
+                    target = "builder",
+                    message = format!("Couldn't build for the client side:  {e}"),
+                );
+                return Err(anyhow!("Couldn't continue building process."));
+            };
 
-    let server_configs = ServerConfigs {
-        port: 8080,
-        _enable_http_logging: true,
-        root_path: &current_dir()?,
+            if let Err(e) = ServerSideBuilder::new("", &out_dir)?.build() {
+                error!(
+                    target = "builder",
+                    message = format!("Couldn't build for the server side: {e}"),
+                );
+                return Err(anyhow!("Couldn't continue building process."));
+            }
+
+            // Waiting till metacall destroyed
+            if (_metacall.0)() == 0 {
+                info!(
+                    target = "builder",
+                    message = "Building is completed",
+                    time = format!("{}ms", instant.elapsed().as_millis())
+                );
+            }
+        }
+        Some(Commands::Run { port }) => {
+            let server_configs = ServerConfigs {
+                port,
+                _enable_http_logging: allow_http_debug,
+                root_path: &current_dir()?,
+            };
+
+            Server::new(&server_configs).run().await?;
+        }
+        _ => {}
     };
 
-    Server::new(&server_configs).run().await?;
     Ok(())
 }
