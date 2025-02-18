32f88233a09208495146b4f9aacb293ba2ba5a93
fahdfady <fahd.fady212@gmail.com>
2025-02-18 20:40:41 +0200

    fix watching events nad logging them

--- crates/metassr-server/src/watcher.rs
+++ crates/metassr-server/src/watcher.rs
@@ -1,4 +1,5 @@
-use notify::{event, Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
+use chrono::Date;
+use notify::{Config, Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 use std::{
     path::Path,
     sync::mpsc::{self, Receiver},
@@ -16,19 +17,23 @@ impl FileWatcher {
         // distributing file events to multiple subscribers
         let (sender, _) = broadcast::channel(100);
         let tx = sender.clone();
-
         let (notify_tx, notify_rx) = mpsc::channel();
 
         std::thread::spawn(move || {
             while let Ok(event) = notify_rx.recv() {
-                let _ = tx.send(event);
+                if is_relevant_event(&event) {
+                    let _ = tx.send(event);
+                }
             }
         });
 
         let watcher = RecommendedWatcher::new(
-            move |res| match res {
+            move |res: Result<Event, notify::Error>| match res {
                 Ok(event) => {
-                    println!("Event: {event:?}");
+                    if is_relevant_event(&event) {
+                        println!("Detected change: {:?}", event);
+                        let _ = notify_tx.send(event);
+                    }
                 }
                 Err(errr) => {
                     eprintln!("Error: {}", errr);
@@ -49,4 +54,41 @@ impl FileWatcher {
     pub fn subscribe(&self) -> broadcast::Receiver<Event> {
         self.sender.subscribe()
     }
+}
+
+// Helper functions
+
+fn is_relevant_event(event: &Event) -> bool {
+    use notify::event::{CreateKind, Event, EventKind, ModifyKind};
+    use notify::EventKind::*;
+
+    match event.kind {
+        Create(_) => true,
+        Modify(ModifyKind::Data(_)) => true,
+        Modify(ModifyKind::Name(_)) => true,
+        Remove(_) => true,
+        _ => false,
+    }
+}
+
+fn format_event(event: &Event) -> String {
+    use notify::event::{CreateKind, Event, EventKind, ModifyKind};
+    use notify::EventKind::*;
+
+    let action = match event.kind {
+        Create(_) => "created",
+        Modify(ModifyKind::Data(_)) => "modified",
+        Modify(ModifyKind::Name(_)) => "renamed",
+        Remove(_) => "deleted",
+        _ => "unknown action",
+    };
+
+    let paths = event
+        .paths
+        .iter()
+        .map(|p| p.display().to_string())
+        .collect::<Vec<_>>()
+        .join(", ");
+
+    format!("{} {}", action, paths)
 }
