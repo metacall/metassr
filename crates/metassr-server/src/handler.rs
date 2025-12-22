bb212bb61a3f9fb73b8845b362343f2dda5f70c4
hulxv <hulxxv@gmail.com>
2025-12-22 23:51:42 +0200

    refactor: remove debugging statements from handler closure

--- crates/metassr-server/src/handler.rs
+++ crates/metassr-server/src/handler.rs
@@ -47,9 +47,8 @@ impl<'a, S: Clone + Send + Sync + 'static> PagesHandler<'a, S> {
             };
 
             let handler =
-                move |Query(params): Query<HashMap<String, String>>,
-                      Path(path): Path<HashMap<String, String>>| async move {
-                    // dbg!(&params, &path);
+                move |Query(_params): Query<HashMap<String, String>>,
+                      Path(_path): Path<HashMap<String, String>>| async move {
                     Html(*html)
                 };
 
