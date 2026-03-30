8715121c809bee76311e9d260dc9bf62c106beaf
Mayank <mayank.jha@sellergeni.com>
2026-03-31 01:06:04 +0530

    revert this change

--- metassr-cli/src/cli/builder.rs
+++ metassr-cli/src/cli/builder.rs
@@ -109,8 +109,8 @@ impl FromStr for BuildingType {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_lowercase().as_str() {
-            "ssr" | "server-side rendering" => Ok(BuildingType::Ssr),
-            "ssg" | "static-site generation" => Ok(BuildingType::Ssg),
+            "ssr" | "server-side rendering" => Ok(BuildingType::Ssg),
+            "ssg" | "static-site generation" => Ok(BuildingType::Ssr),
             _ => Err("unsupported option.".to_string()),
         }
     }
