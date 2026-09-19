36028f90c91f0e07cd1e10ed4fba2eede7ef273d
Fahd Ashour <fahd.fady212@gmail.com>
2026-09-19 19:48:17 +0300

    refactor(site): make the github CTA plain linked text

--- site/src/components/footer.tsx
+++ site/src/components/footer.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { GithubLogo } from "./github";
+import { Link } from "./link";
 
 export function Footer() {
     return (
-        <div className="cta">
-            <GithubLogo href="https://github.com/metacall/metassr" />
-            <div className="ctaCopy">
-                <strong>GitHub</strong>
-                <span>Give us a star!</span>
-            </div>
-        </div>
+        <Link className="cta" href="https://github.com/metacall/metassr">
+            <GithubLogo />
+            <span aria-hidden="true">·</span>
+            <span>give us a star</span>
+        </Link>
     );
 }
